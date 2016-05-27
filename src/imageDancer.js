a6cbed2637336f59c529e0116c859fe1cc5e5bda
Anil Pai <anilbpai@gmail.com>
2016-05-27 13:00:29 -0400

    Pair Programming with Yongfu
    05/21 Sprint

--- src/imageDancer.js
+++ src/imageDancer.js
@@ -1,17 +1,16 @@
-var BlinkyDancer = function(top, left, timeBetweenSteps){
-  Dancer.apply(this, arguments);
+var ImageDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps, "imageDancer");
 };
 
-  BlinkyDancer.prototype = Object.create(Dancer.prototype);
-  BlinkyDancer.prototype.constructor = BlinkyDancer;
+  ImageDancer.prototype = Object.create(Dancer.prototype);
+  ImageDancer.prototype.constructor = ImageDancer;
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  BlinkyDancer.prototype.step = function(){
+  ImageDancer.prototype.step = function(){
     Dancer.prototype.step.call(this);
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-    this.$node.toggle();
   };
