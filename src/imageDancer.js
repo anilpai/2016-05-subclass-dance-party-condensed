da29e6f70a4ca44d105ba0afc42a1b0a6ea35360
Yongfu <yongfu@novc02ff2x0df93.ads.autodesk.com>
2016-05-27 13:43:56 -0400

    some visual change

--- src/imageDancer.js
+++ src/imageDancer.js
@@ -7,7 +7,4 @@ ImageDancer.prototype.constructor = ImageDancer;
 
 ImageDancer.prototype.step = function(){
   Dancer.prototype.step.call(this);
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-  };
+};
--- src/movableBlinkyDancer.js
+++ src/movableBlinkyDancer.js
@@ -1,24 +1,19 @@
 var MovableBlinkyDancer = function(top, left, timeBetweenSteps){
-  BlinkyDancer.apply(this, arguments);
+  BlinkyDancer.call(this, top, left, timeBetweenSteps, "movableDancer");
   this.direction = 1;
 };
 
-  MovableBlinkyDancer.prototype = Object.create(BlinkyDancer.prototype);
-  MovableBlinkyDancer.prototype.constructor = MovableBlinkyDancer;
+MovableBlinkyDancer.prototype = Object.create(BlinkyDancer.prototype);
+MovableBlinkyDancer.prototype.constructor = MovableBlinkyDancer;
 
-  MovableBlinkyDancer.prototype.step = function(){
-    BlinkyDancer.prototype.step.call(this);
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    //this.$node.toggle();
-    if(this.left >= $("body").width()){
-      this.direction = -1;
-    }else if(this.left < 0){
-      this.direction = 1;
-    }
-    var step = 10;
-    var newLeft = this.left + (this.direction*step);
-    this.setPosition(this.top, newLeft);
-    
-  };
+MovableBlinkyDancer.prototype.step = function(){
+  BlinkyDancer.prototype.step.call(this);
+  if(this.left >= $("body").width()) {
+    this.direction = -1;
+  } else if(this.left < 0){
+    this.direction = 1;
+  }
+  var step = 10;
+  var newLeft = this.left + (this.direction*step);
+  this.setPosition(this.top, newLeft);  
+};
